f615351fb522faa9f49c0655760608cd7bbe276c
carbon14 <10398567+david-c14@users.noreply.github.com>
2018-03-21 15:56:57 +0000

    Add 9 more BP-1xx models

--- src/SubmarineFree.hpp
+++ src/SubmarineFree.hpp
@@ -7,4 +7,13 @@ using namespace rack;
 extern Plugin *plugin;
 
 // Forward-declare each Model, defined in each module source file
+extern Model *modelBP101;
+extern Model *modelBP102;
+extern Model *modelBP104;
+extern Model *modelBP108;
 extern Model *modelBP110;
+extern Model *modelBP112;
+extern Model *modelBP116;
+extern Model *modelBP120;
+extern Model *modelBP124;
+extern Model *modelBP132;
