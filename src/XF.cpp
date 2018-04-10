48eaa14ac196038834c5a7cab75642609d7d07f1
carbon14 <10398567+david-c14@users.noreply.github.com>
2018-04-10 15:34:24 +0100

    Add autogenerated lists as a comment to the XF source code
    These autogenerated lists were built using the preprocessor and will
    be copied into various parts of the source code at a later point.
    
    They have been included in this file for safekeeping in the interim.

--- src/XF.cpp
+++ src/XF.cpp
@@ -201,3 +201,268 @@ struct XF101 : ModuleWidget {
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
 Model *modelXF101 = Model::create<XF_101, XF101>("SubmarineFree", "XF-101", "XF-101 1-Channel Cross Fader", BLANK_TAG);
+
+/*
+debug("101");
+
+ enum ParamIds {
+  PARAM_CV_1,
+  PARAM_MODE_1,
+  PARAM_FADE_1,
+  NUM_PARAMS
+ };
+ enum InputIds {
+  INPUT_A_1,
+  INPUT_B_1,
+  INPUT_CV_1,
+  NUM_INPUTS
+ };
+ enum OutputIds {
+  OUTPUT_1,
+  NUM_OUTPUTS
+ };
+ enum LightIds {;
+  LIGHT_LIN_1,
+  LIGHT_LOG_1,
+  LIGHT_AUTO_1, LIGHT_INV_1,
+  NUM_LIGHTS
+ };
+
+debug("102");
+
+ enum ParamIds {
+  PARAM_CV_1, PARAM_CV_2,
+  PARAM_MODE_1, PARAM_MODE_2,
+  PARAM_FADE_1, PARAM_FADE_2,
+  PARAM_LINK_1,
+  NUM_PARAMS
+ };
+ enum InputIds {
+  INPUT_A_1, INPUT_A_2,
+  INPUT_B_1, INPUT_B_2,
+  INPUT_CV_1, INPUT_CV_2,
+  NUM_INPUTS
+ };
+ enum OutputIds {
+  OUTPUT_1, OUTPUT_2,
+  NUM_OUTPUTS
+ };
+ enum LightIds {;
+  LIGHT_LIN_1, LIGHT_LIN_2,
+  LIGHT_LOG_1, LIGHT_LOG_2,
+  LIGHT_AUTO_1, LIGHT_INV_1, LIGHT_AUTO_2, LIGHT_INV_2,
+  NUM_LIGHTS
+ };
+
+debug("104");
+
+ enum ParamIds {
+  PARAM_CV_1, PARAM_CV_2, PARAM_CV_3, PARAM_CV_4,
+  PARAM_MODE_1, PARAM_MODE_2, PARAM_MODE_3, PARAM_MODE_4,
+  PARAM_FADE_1, PARAM_FADE_2, PARAM_FADE_3, PARAM_FADE_4,
+  PARAM_LINK_1, PARAM_LINK_2,
+  NUM_PARAMS
+ };
+ enum InputIds {
+  INPUT_A_1, INPUT_A_2, INPUT_A_3, INPUT_A_4,
+  INPUT_B_1, INPUT_B_2, INPUT_B_3, INPUT_B_4,
+  INPUT_CV_1, INPUT_CV_2, INPUT_CV_3, INPUT_CV_4,
+  NUM_INPUTS
+ };
+ enum OutputIds {
+  OUTPUT_1, OUTPUT_2, OUTPUT_3, OUTPUT_4,
+  NUM_OUTPUTS
+ };
+ enum LightIds {;
+  LIGHT_LIN_1, LIGHT_LIN_2, LIGHT_LIN_3, LIGHT_LIN_4,
+  LIGHT_LOG_1, LIGHT_LOG_2, LIGHT_LOG_3, LIGHT_LOG_4,
+  LIGHT_AUTO_1, LIGHT_INV_1, LIGHT_INV_2, LIGHT_INV_2, LIGHT_AUTO_3, LIGHT_INV_3, LIGHT_AUTO_4, LIGHT_INV_4
+  NUM_LIGHTS
+ };
+
+debug("201");
+
+ enum ParamIds {
+  PARAM_CV_1,
+  PARAM_MODE_1,
+  PARAM_FADE_1,
+  NUM_PARAMS
+ };
+ enum InputIds {
+  INPUT_A_1, INPUT_AR_1,
+  INPUT_B_1, INPUT_BR_1,
+  INPUT_CV_1,
+  NUM_INPUTS
+ };
+ enum OutputIds {
+  OUTPUT_1, OUTPUTR_1,
+  NUM_OUTPUTS
+ };
+ enum LightIds {;
+  LIGHT_LIN_1,
+  LIGHT_LOG_1,
+  LIGHT_AUTO_1, LIGHT_INV_1,
+  NUM_LIGHTS
+ };
+
+debug("202");
+
+ enum ParamIds {
+  PARAM_CV_1, PARAM_CV_2,
+  PARAM_MODE_1, PARAM_MODE_2,
+  PARAM_FADE_1, PARAM_FADE_2,
+  NUM_PARAMS
+ };
+ enum InputIds {
+  INPUT_A_1, INPUT_A_2, INPUT_AR_1, INPUT_AR_2,
+  INPUT_B_1, INPUT_B_2, INPUT_BR_1, INPUT_BR_2,
+  INPUT_CV_1, INPUT_CV_2,
+  NUM_INPUTS
+ };
+ enum OutputIds {
+  OUTPUT_1, OUTPUT_2, OUTPUTR_1, OUTPUTR_2,
+  NUM_OUTPUTS
+ };
+ enum LightIds {;
+  LIGHT_LIN_1, LIGHT_LIN_2,
+  LIGHT_LOG_1, LIGHT_LOG_2,
+  LIGHT_AUTO_1, LIGHT_INV_1, LIGHT_AUTO_2, LIGHT_INV_2,
+  NUM_LIGHTS
+ };
+
+  {
+   INPUT_A_1,
+   0,
+   INPUT_B_1,
+   0,
+   PARAM_FADE_1,
+   INPUT_CV_1,
+   OUTPUT_1,
+   0,
+   PARAM_CV_1,
+   PARAM_MODE_1,
+   LIGHT_LIN_1,
+   LIGHT_LOG_1,
+   LIGHT_AUTO_1,
+   &correlators[0]
+  }
+,
+  {
+   INPUT_A_2,
+   0,
+   INPUT_B_2,
+   0,
+   PARAM_FADE_2,
+   INPUT_CV_2,
+   OUTPUT_2,
+   0,
+   PARAM_CV_2,
+   PARAM_MODE_2,
+   LIGHT_LIN_2,
+   LIGHT_LOG_2,
+   LIGHT_AUTO_2,
+   &correlators[1]
+  }
+,
+  {
+   INPUT_A_3,
+   0,
+   INPUT_B_3,
+   0,
+   PARAM_FADE_3,
+   INPUT_CV_3,
+   OUTPUT_3,
+   0,
+   PARAM_CV_3,
+   PARAM_MODE_3,
+   LIGHT_LIN_3,
+   LIGHT_LOG_3,
+   LIGHT_AUTO_3,
+   &correlators[2]
+  }
+,
+  {
+   INPUT_A_4,
+   0,
+   INPUT_B_4,
+   0,
+   PARAM_FADE_4,
+   INPUT_CV_4,
+   OUTPUT_4,
+   0,
+   PARAM_CV_4,
+   PARAM_MODE_4,
+   LIGHT_LIN_4,
+   LIGHT_LOG_4,
+   LIGHT_AUTO_4,
+   &correlators[3]
+  }
+,
+  {
+   INPUT_A_1,
+   INPUT_A_2,
+   INPUT_B_1,
+   INPUT_B_2,
+   PARAM_FADE_1,
+   INPUT_CV_1,
+   OUTPUT_1,
+   OUTPUT_2,
+   PARAM_CV_1,
+   PARAM_MODE_1,
+   LIGHT_LIN_1,
+   LIGHT_LOG_1,
+   LIGHT_AUTO_1,
+   &correlators[0]
+  }
+,
+  {
+   INPUT_A_3,
+   INPUT_A_4,
+   INPUT_B_3,
+   INPUT_B_4,
+   PARAM_FADE_3,
+   INPUT_CV_3,
+   OUTPUT_3,
+   OUTPUT_4,
+   PARAM_CV_3,
+   PARAM_MODE_3,
+   LIGHT_LIN_3,
+   LIGHT_LOG_3,
+   LIGHT_AUTO_3,
+   &correlators[2]
+  }
+
+  {
+   INPUT_A_1,
+   INPUT_AR_1,
+   INPUT_B_1,
+   INPUT_BR_1,
+   PARAM_FADE_1,
+   INPUT_CV_1,
+   OUTPUT_1,
+   OUTPUTR_1,
+   PARAM_CV_1,
+   PARAM_MODE_1,
+   LIGHT_LIN_1,
+   LIGHT_LOG_1,
+   LIGHT_AUTO_1,
+   &correlators[0]
+  }
+,
+  {
+   INPUT_A_2,
+   INPUT_AR_2,
+   INPUT_B_2,
+   INPUT_BR_2,
+   PARAM_FADE_2,
+   INPUT_CV_2,
+   OUTPUT_2,
+   OUTPUTR_2,
+   PARAM_CV_2,
+   PARAM_MODE_2,
+   LIGHT_LIN_2,
+   LIGHT_LOG_2,
+   LIGHT_AUTO_2,
+   &correlators[1]
+  }
+*/
