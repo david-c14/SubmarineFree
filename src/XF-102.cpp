89a426a15a26a540c43e1496abf2b978c535f320
david-c14 <10398567+david-c14@users.noreply.github.com>
2018-04-14 11:42:42 +0100

    Use loop to set widget positions and control arrays

--- src/XF-102.cpp
+++ src/XF-102.cpp
@@ -1,6 +1,7 @@
 #include "XF.hpp"
 
 struct XF_102 : XF {
+	static const int deviceCount = 2;
 	enum ParamIds {
 		PARAM_CV_1, PARAM_CV_2,
 		PARAM_MODE_1, PARAM_MODE_2,
@@ -24,60 +25,45 @@ struct XF_102 : XF {
 		LIGHT_AUTO_1, LIGHT_INV_1, LIGHT_AUTO_2, LIGHT_INV_2,
 		NUM_LIGHTS
 	};
-	char faderKnob_enabled[2];
-	XF_Correlator correlators[2];
-	XF_Controls controls[3] = {
-		{
-			INPUT_A_1,
-			0,
-			INPUT_B_1,
-			0,
-			PARAM_FADE_1,
-			INPUT_CV_1,
-			OUTPUT_1,
-			0,
-			PARAM_CV_1,
-			PARAM_MODE_1,
-			LIGHT_LIN_1,
-			LIGHT_LOG_1,
-			LIGHT_AUTO_1,
-			&correlators[0]
-		},
-		{
-			INPUT_A_2,
-			0,
-			INPUT_B_2,
-			0,
-			PARAM_FADE_2,
-			INPUT_CV_2,
-			OUTPUT_2,
-			0,
-			PARAM_CV_2,
-			PARAM_MODE_2,
-			LIGHT_LIN_2,
-			LIGHT_LOG_2,
-			LIGHT_AUTO_2,
-			&correlators[0]
-		},	
-		{
-			INPUT_A_1,
-			INPUT_A_2,
-			INPUT_B_1,
-			INPUT_B_2,
-			PARAM_FADE_1,
-			INPUT_CV_1,
-			OUTPUT_1,
-			OUTPUT_2,
-			PARAM_CV_1,
-			PARAM_MODE_1,
-			LIGHT_LIN_1,
-			LIGHT_LOG_1,
-			LIGHT_AUTO_1,
-			&correlators[0]
-		}
-	};
+	char faderKnob_enabled[deviceCount];
+	XF_Correlator correlators[deviceCount];
+	XF_Controls controls[(int)(deviceCount * 1.5f)];
 
-	XF_102() : XF(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	XF_102() : XF(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+		for (int i = 0; i < deviceCount; i++) {
+			controls[i].a = INPUT_A_1 + i;
+			controls[i].ar = 0;
+			controls[i].b = INPUT_B_1 + i;
+			controls[i].br = 0;
+			controls[i].fader = PARAM_FADE_1 + i;
+			controls[i].cv = INPUT_CV_1 + i;
+			controls[i].out = OUTPUT_1 + i;
+			controls[i].outr = 0;
+			controls[i].polar = PARAM_CV_1 + i;
+			controls[i].mode = PARAM_MODE_1 + i;
+			controls[i].light1 = LIGHT_LIN_1 + i;
+			controls[i].light2 = LIGHT_LOG_1 + i;
+			controls[i].light3 = LIGHT_AUTO_1 + i * 2;
+			controls[i].correlator = &correlators[i];
+		}
+		for (int i = 0; i < deviceCount / 2; i++) {
+			int x = i * 2;
+			controls[i + deviceCount].a = INPUT_A_1 + x;
+			controls[i + deviceCount].ar = INPUT_A_2 + x;
+			controls[i + deviceCount].b = INPUT_B_1 + x;
+			controls[i + deviceCount].br = INPUT_B_2 + x;
+			controls[i + deviceCount].fader = PARAM_FADE_1 + x;
+			controls[i + deviceCount].cv = INPUT_CV_1 + x;
+			controls[i + deviceCount].out = OUTPUT_1 + x;
+			controls[i + deviceCount].outr = OUTPUT_2 + x;
+			controls[i + deviceCount].polar = PARAM_CV_1 + x;
+			controls[i + deviceCount].mode = PARAM_MODE_1 + x;
+			controls[i + deviceCount].light1 = LIGHT_LIN_1 + x;
+			controls[i + deviceCount].light2 = LIGHT_LOG_1 + x;
+			controls[i + deviceCount].light3 = LIGHT_AUTO_1 + x * 2;
+			controls[i + deviceCount].correlator = &correlators[x];
+		}
+	}
 	void step() override;
 };
 
@@ -98,39 +84,26 @@ struct XF102 : ModuleWidget {
 	XF102(XF_102 *module) : ModuleWidget(module) {
 		LightKnob *fader;
 		setPanel(SVG::load(assetPlugin(plugin, "res/XF-104.svg")));
+		for (int i = 0; i < XF_102::deviceCount; i++) {
+			int offset = 88 * i;
+			addInput(Port::create<PJ301MPort>(Vec(27.5,18 + offset), Port::INPUT, module, XF_102::INPUT_A_1 + i));
+			addInput(Port::create<sub_port>(Vec(127.5,18 + offset), Port::INPUT, module, XF_102::INPUT_B_1 + i));
+			addInput(Port::create<sub_port>(Vec(27.5,74 + offset), Port::INPUT, module, XF_102::INPUT_CV_1 + i));
 
-		addInput(Port::create<PJ301MPort>(Vec(27.5,18), Port::INPUT, module, XF_102::INPUT_A_1));
-		addInput(Port::create<sub_port>(Vec(127.5,18), Port::INPUT, module, XF_102::INPUT_B_1));
-		addInput(Port::create<sub_port>(Vec(27.5,74), Port::INPUT, module, XF_102::INPUT_CV_1));
+			addOutput(Port::create<sub_port>(Vec(127.5,74 + offset), Port::OUTPUT, module, XF_102::OUTPUT_1 + i));
 
-		addOutput(Port::create<sub_port>(Vec(127.5,74), Port::OUTPUT, module, XF_102::OUTPUT_1));
+			addParam(ParamWidget::create<sub_sw_2>(Vec(41, 46 + offset), module, XF_102::PARAM_CV_1 + i, 0.0f, 1.0f, 0.0f));
+			addParam(ParamWidget::create<sub_sw_3>(Vec(125, 43.5 + offset), module, XF_102::PARAM_MODE_1 + i, 0.0f, 2.0f, 0.0f));
+			fader = ParamWidget::create<sub_knob_large>(Vec(63, 31 + offset), module, XF_102::PARAM_FADE_1 + i, 0.0f, 10.0f, 5.0f);
+			fader->moduleFlag = &module->faderKnob_enabled[i];
+			addParam(fader);
 
-		addParam(ParamWidget::create<sub_sw_2>(Vec(41, 46), module, XF_102::PARAM_CV_1, 0.0f, 1.0f, 0.0f));
-		addParam(ParamWidget::create<sub_sw_3>(Vec(125, 43.5), module, XF_102::PARAM_MODE_1, 0.0f, 2.0f, 0.0f));
+			addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 47 + offset), module, XF_102::LIGHT_LIN_1 + i));
+			addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 57 + offset), module, XF_102::LIGHT_LOG_1 + i));
+			addChild(ModuleLightWidget::create<TinyLight<BlueRedLight>>(Vec(141, 67 + offset), module, XF_102::LIGHT_AUTO_1 + i * 2));
+		}
+
 		addParam(ParamWidget::create<sub_btn>(Vec(90, 94.5), module, XF_102::PARAM_LINK_1, 0.0f, 1.0f, 0.0f));
-		fader = ParamWidget::create<sub_knob_large>(Vec(63, 31), module, XF_102::PARAM_FADE_1, 0.0f, 10.0f, 5.0f);
-		fader->moduleFlag = &module->faderKnob_enabled[0];
-		addParam(fader);
-
-		addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 47), module, XF_102::LIGHT_LIN_1));
-		addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 57), module, XF_102::LIGHT_LOG_1));
-		addChild(ModuleLightWidget::create<TinyLight<BlueRedLight>>(Vec(141, 67), module, XF_102::LIGHT_AUTO_1));
-
-		addInput(Port::create<PJ301MPort>(Vec(27.5,106), Port::INPUT, module, XF_102::INPUT_A_2));
-		addInput(Port::create<sub_port>(Vec(127.5,106), Port::INPUT, module, XF_102::INPUT_B_2));
-		addInput(Port::create<sub_port>(Vec(27.5,162), Port::INPUT, module, XF_102::INPUT_CV_2));
-
-		addOutput(Port::create<sub_port>(Vec(127.5,162), Port::OUTPUT, module, XF_102::OUTPUT_2));
-
-		addParam(ParamWidget::create<sub_sw_2>(Vec(41, 134), module, XF_102::PARAM_CV_2, 0.0f, 1.0f, 0.0f));
-		addParam(ParamWidget::create<sub_sw_3>(Vec(125, 131.5), module, XF_102::PARAM_MODE_2, 0.0f, 2.0f, 0.0f));
-		fader = ParamWidget::create<sub_knob_large>(Vec(63, 119), module, XF_102::PARAM_FADE_2, 0.0f, 10.0f, 5.0f);
-		fader->moduleFlag = &module->faderKnob_enabled[1];
-		addParam(fader);
-
-		addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 135), module, XF_102::LIGHT_LIN_2));
-		addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(141, 145), module, XF_102::LIGHT_LOG_2));
-		addChild(ModuleLightWidget::create<TinyLight<BlueRedLight>>(Vec(141, 155), module, XF_102::LIGHT_AUTO_2));
 	}
 };
 
