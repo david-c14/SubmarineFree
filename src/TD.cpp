15e74f61cadf898eb5ad47148d366a7e1ea9200f
david-c14 <10398567+david-c14@users.noreply.github.com>
2018-06-19 19:37:06 +0100

    Finished code for TF text formatting module

--- src/TD.cpp
+++ src/TD.cpp
@@ -1,4 +1,5 @@
 #include "SubmarineFree.hpp"
+#include "window.hpp"
 #include "torpedo.hpp"
 
 struct TD_116;
@@ -7,6 +8,7 @@ struct TDInput : Torpedo::PatchInputPort {
 	TD_116 *tdModule;
 	TDInput(TD_116 *module, unsigned int portNum) : Torpedo::PatchInputPort((Module *)module, portNum) { tdModule = module; }
 	void received(std::string pluginName, std::string moduleName, json_t *rootJ) override;
+	NVGcolor decodeColor(std::string colorStr);
 };
 
 struct TD_116 : Module {
@@ -27,15 +29,19 @@ struct TD_116 : Module {
 	}
 	std::string text;
 	int fontSize = 12;
+	NVGcolor fg = nvgRGB(0x28, 0xb0, 0xf3);
+	NVGcolor bg = nvgRGB(0,0,0);
 	int isDirty = false;
 	int isDirtyC = false;
 };
 
 struct TDText : LedDisplayTextField {
 	TD_116 *tdModule;
-	std::shared_ptr<Font> font = Font::load(assetGlobal("res/fonts/DejaVuSans.ttf"));
 	NVGcolor bgColor = nvgRGB(0x00, 0x00, 0x00);
 	int fontSize = 12;
+	TDText() {
+		color = nvgRGB(0x28, 0xb0, 0xf3);
+	}
 	void onTextChange() override {
 		LedDisplayTextField::onTextChange();
 		tdModule->sendText(text);
@@ -59,11 +65,18 @@ struct TDText : LedDisplayTextField {
 			bndIconLabelCaret(vg, textOffset.x, textOffset.y,
 				box.size.x - 2*textOffset.x, box.size.y - 2*textOffset.y,
 				-1, color, fontSize, text.c_str(), highlightColor, begin, end);
-			bndSetFont(font->handle);
 		}
 		nvgResetScissor(vg);
+		bndSetFont(gGuiFont->handle);
 	}
 };
+
+NVGcolor TDInput::decodeColor(std::string colorStr) {
+	int r = (colorStr[0] - 'A') * 16 + (colorStr[1] - 'A');
+	int g = (colorStr[2] - 'A') * 16 + (colorStr[3] - 'A');
+	int b = (colorStr[4] - 'A') * 16 + (colorStr[5] - 'A');
+	return nvgRGB(r, g, b);
+}
 
 void TDInput::received(std::string pluginName, std::string moduleName, json_t *rootJ) {
 	if (pluginName.compare("SubmarineFree")) return;
@@ -80,6 +93,16 @@ void TDInput::received(std::string pluginName, std::string moduleName, json_t *r
 			tdModule->fontSize = json_number_value(size);
 			tdModule->isDirtyC = true;
 		}	
+		json_t *fg = json_object_get(rootJ, "fg");
+		if (fg) {
+			tdModule->fg = decodeColor(std::string(json_string_value(fg)));
+			tdModule->isDirtyC = true;
+		}
+		json_t *bg = json_object_get(rootJ, "bg");
+		if (bg) {
+			tdModule->bg = decodeColor(std::string(json_string_value(bg)));
+			tdModule->isDirtyC = true;
+		}
 	}	
 }
 
@@ -93,7 +116,7 @@ struct TD116 : ModuleWidget {
 		addOutput(Port::create<sub_port_black>(Vec(211,19), Port::OUTPUT, module, 0));	
 
 		textField = Widget::create<TDText>(mm2px(Vec(3.39962, 15.8373)));
-		textField->box.size = mm2px(Vec(74.480, 100.753));
+		textField->box.size = mm2px(Vec(74.480, 102.753));
 		textField->multiline = true;
 		textField->tdModule = module;
 		addChild(textField);
@@ -102,8 +125,10 @@ struct TD116 : ModuleWidget {
 	json_t *toJson() override {
 		json_t *rootJ = ModuleWidget::toJson();
 
-		// text
 		json_object_set_new(rootJ, "text", json_string(textField->text.c_str()));
+		json_object_set_new(rootJ, "size", json_real(textField->fontSize));
+		json_object_set_new(rootJ, "fg", colorToJson(textField->color));
+		json_object_set_new(rootJ, "bg", colorToJson(textField->bgColor));
 
 		return rootJ;
 	}
@@ -111,10 +136,18 @@ struct TD116 : ModuleWidget {
 	void fromJson(json_t *rootJ) override {
 		ModuleWidget::fromJson(rootJ);
 
-		// text
 		json_t *textJ = json_object_get(rootJ, "text");
 		if (textJ)
 			textField->text = json_string_value(textJ);
+		json_t *sizeJ = json_object_get(rootJ, "size");
+		if (sizeJ)
+			textField->fontSize = json_number_value(sizeJ);
+		json_t *fgJ = json_object_get(rootJ, "fg");
+		if (fgJ)
+			textField->color = jsonToColor(fgJ);
+		json_t *bgJ = json_object_get(rootJ, "bg");
+		if (bgJ)
+			textField->bgColor = jsonToColor(bgJ);
 	}
 
 	void step() override {
@@ -125,9 +158,19 @@ struct TD116 : ModuleWidget {
 		}
 		if (tdModule->isDirtyC) {
 			textField->fontSize = tdModule->fontSize;
+			textField->color = tdModule->fg;
+			textField->bgColor = tdModule->bg;
 			tdModule->isDirtyC = false;
 		}
 		ModuleWidget::step();
+	}
+
+	void reset() override {
+		textField->fontSize = 12;
+		textField->text = "";
+		textField->color = nvgRGB(0x28, 0xb0, 0xf3);
+		textField->bgColor = nvgRGB(0,0,0);	
+		ModuleWidget::reset();
 	}
 };
 
--- src/TF-101.cpp
+++ src/TF-101.cpp
@@ -38,9 +38,25 @@ struct TF_101 : Module  {
 
 	float prevValues[7];
 	int isDirty = false;
-	TF_101() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	TF_101() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+		prevValues[0] = 0.1569f;
+		prevValues[1] = 0.6902f;
+		prevValues[2] = 0.9529f;
+		prevValues[6] = 12.0f;
+	}
 	Torpedo::PatchOutputPort outPort = Torpedo::PatchOutputPort(this, OUTPUT_TOR);	
 	void step() override;
+	std::string encodeColor(float r, float g, float b) {
+		std::string out;
+		out.push_back('A'+(int)(r * 255) / 16);	
+		out.push_back('A'+(int)(r * 255) % 16);	
+		out.push_back('A'+(int)(g * 255) / 16);	
+		out.push_back('A'+(int)(g * 255) % 16);	
+		out.push_back('A'+(int)(b * 255) / 16);	
+		out.push_back('A'+(int)(b * 255) % 16);	
+	
+		return out;
+	}
 };
 
 void TF_101::step() {
@@ -52,7 +68,7 @@ void TF_101::step() {
 			prevValues[i] = newValue;
 		}
 	}
-	float newValue = clamp(params[PARAM_FONT_SIZE].value + inputs[INPUT_FONT_SIZE].value / 10.0f, 6.0f, 28.0f); 
+	float newValue = clamp(params[PARAM_FONT_SIZE].value + inputs[INPUT_FONT_SIZE].value * 2.0f, 6.0f, 26.0f); 
 	if (prevValues[6] != newValue) {
 		isDirty = true;
 		prevValues[6] = newValue;
@@ -60,8 +76,8 @@ void TF_101::step() {
 	if (isDirty) {
 		isDirty = false;
 		json_t *rootJ = json_object();
-		json_object_set_new(rootJ, "fg", colorToJson(nvgRGBf(prevValues[0], prevValues[1], prevValues[2])));
-		json_object_set_new(rootJ, "bg", colorToJson(nvgRGBf(prevValues[3], prevValues[4], prevValues[5])));
+		json_object_set_new(rootJ, "fg", json_string(encodeColor(prevValues[0], prevValues[1], prevValues[2]).c_str()));
+		json_object_set_new(rootJ, "bg", json_string(encodeColor(prevValues[3], prevValues[4], prevValues[5]).c_str()));
 		json_object_set_new(rootJ, "size", json_real(prevValues[6]));
 		outPort.send("SubmarineFree", "TDNotesColor", rootJ);
 	}
@@ -89,13 +105,13 @@ struct TF101 : ModuleWidget {
 		addInput(Port::create<sub_port>(Vec(4,205), Port::INPUT, module, TF_101::INPUT_BG_BLUE));
 		addInput(Port::create<sub_port>(Vec(4,245), Port::INPUT, module, TF_101::INPUT_FONT_SIZE));
 
-		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 25.5), module, TF_101::PARAM_FG_RED, 0.0f, 1.0f, 0.0f));
-		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 60.5), module, TF_101::PARAM_FG_GREEN, 0.0f, 1.0f, 0.0f));
-		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 95.5), module, TF_101::PARAM_FG_BLUE, 0.0f, 1.0f, 0.0f));
+		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 25.5), module, TF_101::PARAM_FG_RED, 0.0f, 1.0f, 0.1569f));
+		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 60.5), module, TF_101::PARAM_FG_GREEN, 0.0f, 1.0f, 0.6902f));
+		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 95.5), module, TF_101::PARAM_FG_BLUE, 0.0f, 1.0f, 0.9529f));
 		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 135.5), module, TF_101::PARAM_BG_RED, 0.0f, 1.0f, 0.0f));
 		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 170.5), module, TF_101::PARAM_BG_GREEN, 0.0f, 1.0f, 0.0f));
 		addParam(ParamWidget::create<sub_knob_small>(Vec(33, 205.5), module, TF_101::PARAM_BG_BLUE, 0.0f, 1.0f, 0.0f));
-		addParam(ParamWidget::create<sub_knob_small_snap>(Vec(33, 245.5), module, TF_101::PARAM_FONT_SIZE, 6.0f, 28.0f, 12.0f));
+		addParam(ParamWidget::create<sub_knob_small_snap>(Vec(33, 245.5), module, TF_101::PARAM_FONT_SIZE, 6.0f, 26.0f, 12.0f));
 
 		addChild(ModuleLightWidget::create<MediumLight<WhiteLight>>(Vec(66, 63), module, TF_101::LIGHT_FG_RED));
 		addChild(ModuleLightWidget::create<MediumLight<WhiteLight>>(Vec(66, 173), module, TF_101::LIGHT_BG_RED));
