b4d81588c0309a6d2ddb6fc855d6643650d4e7d6
carbon14 <10398567+david-c14@users.noreply.github.com>
2019-02-20 15:14:46 +0000

    Replace ugly nvgArc calls in Port with nvgBezierTo.
    These are not so easy to follow, but hopefully will mitigate the graphics issue which is plaguing my ubuntu builds.

--- src/ComponentLibrary/Port.cpp
+++ src/ComponentLibrary/Port.cpp
@@ -51,14 +51,31 @@ void SilverPort::draw(NVGcontext *vg) {
 	{
 		nvgBeginPath(vg);
 //		nvgMoveTo(vg, 0.66556777, 9.9934984);
-		nvgArc(vg, radius, radius, radius - 1, M_PI + 0.2076, -0.2076, NVG_CW); 
-		nvgArc(vg, 24.5, 12.5, 2.5, 1.467 - M_PI, M_PI - 1.467, NVG_CCW);
-		nvgArc(vg, radius, radius, radius - 1, 0.2076, M_PI - 0.2076, NVG_CW);
-		nvgArc(vg, 0.5, 12.5, 2.5, 1.467, -1.467, NVG_CCW);
-		nvgPathWinding(vg, NVG_HOLE);
+		//nvgArc(vg, radius, radius, radius - 1, M_PI + 0.2076, -0.2076, NVG_CW); 
+		//nvgArc(vg, 24.5, 12.5, 2.5, 1.467 - M_PI, M_PI - 1.467, NVG_CCW);
+		//nvgArc(vg, radius, radius, radius - 1, 0.2076, M_PI - 0.2076, NVG_CW);
+		//nvgArc(vg, 0.5, 12.5, 2.5, 1.467, -1.467, NVG_CCW);
+
+
+		nvgMoveTo(vg, 0.665568, 9.993499);
+		nvgBezierTo(vg, 1.849660, 4.402603, 6.784906, 0.403117, 12.499817, 0.403117);
+		nvgBezierTo(vg, 18.214727, 0.403117, 23.149975, 4.402601, 24.334068, 9.993498);
+		nvgBezierTo(vg, 23.438292, 9.993016, 22.610352, 10.470624, 22.162313, 11.246303);
+		nvgBezierTo(vg, 21.714275, 12.021981, 21.714256, 12.977802, 22.162262, 13.753500);
+		nvgBezierTo(vg, 22.610268, 14.529198, 23.438189, 15.006838, 24.333965, 15.006392);
+		nvgBezierTo(vg, 23.149975, 20.597290, 18.214729, 24.596775, 12.499819, 24.596775);
+		nvgBezierTo(vg, 6.784908, 24.596775, 1.849660, 20.597288, 0.665567, 15.006392);
+		nvgBezierTo(vg, 2.049333, 15.005648, 3.170693, 13.883659, 3.170665, 12.499894);
+		nvgBezierTo(vg, 3.170636, 11.116129, 2.049230, 9.994187, 0.665464, 9.993499);
+		nvgBezierTo(vg, 0.665499, 9.993499, 0.665533, 9.993499, 0.665568, 9.993499);
+
+
+
+
+		nvgPathWinding(vg, NVG_SOLID);
 		nvgClosePath(vg);
 		nvgCircle(vg, radius, radius, 8.0);
-		nvgPathWinding(vg, NVG_SOLID);
+		nvgPathWinding(vg, NVG_HOLE);
 		nvgStrokeWidth(vg, 0.80645);
 		if (type == Port::OUTPUT) {
 			if (gScheme.isFlat) {
