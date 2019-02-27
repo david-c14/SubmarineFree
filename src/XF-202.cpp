b597814f9541bf3319568c4c1af732734781ed3a
carbon14 <10398567+david-c14@users.noreply.github.com>
2019-02-27 11:29:44 +0000

    Replace front panel on XF-2xx devices

--- src/XF-202.cpp
+++ src/XF-202.cpp
@@ -1,3 +1,5 @@
+//SubTag W8
+
 #include "XF.hpp"
 
 struct XF_202 : XF {
@@ -56,33 +58,73 @@ void XF_202::step() {
 	crossFade(&controls[1]);
 }
 
-struct XF202 : ModuleWidget {
-	XF202(XF_202 *module) : ModuleWidget(module) {
+struct XF202 : SchemeModuleWidget {
+	XF202(XF_202 *module) : SchemeModuleWidget(module) {
 		XF_LightKnob *fader;
-		setPanel(SVG::load(assetPlugin(plugin, "res/XF-202.svg")));
+		this->box.size = Vec(120, 380);
+		addChild(new SchemePanel(this->box.size));
 		for (int i = 0; i < XF_202::deviceCount; i++) {
 			int offset = 176 * i;
-			addInput(Port::create<SilverPort>(Vec(3,18 + offset), Port::INPUT, module, XF_202::INPUT_A_1 + i));
-			addInput(Port::create<RedPort>(Vec(3,45 + offset), Port::INPUT, module, XF_202::INPUT_AR_1 + i));
-			addInput(Port::create<SilverPort>(Vec(92,18 + offset), Port::INPUT, module, XF_202::INPUT_B_1 + i));
-			addInput(Port::create<RedPort>(Vec(92,45 + offset), Port::INPUT, module, XF_202::INPUT_BR_1 + i));
-			addInput(Port::create<SilverPort>(Vec(3,120 + offset), Port::INPUT, module, XF_202::INPUT_CV_1 + i));
+			addInput(createInputCentered<SilverPort>(Vec(15.5,30.5 + offset), module, XF_202::INPUT_A_1 + i));
+			addInput(createInputCentered<RedPort>(Vec(15.5,57.5 + offset), module, XF_202::INPUT_AR_1 + i));
+			addInput(createInputCentered<SilverPort>(Vec(104.5,30.5 + offset), module, XF_202::INPUT_B_1 + i));
+			addInput(createInputCentered<RedPort>(Vec(104.5,57.5 + offset), module, XF_202::INPUT_BR_1 + i));
+			addInput(createInputCentered<SilverPort>(Vec(15.5,132.5 + offset), module, XF_202::INPUT_CV_1 + i));
 
-			addOutput(Port::create<SilverPort>(Vec(92,93 + offset), Port::OUTPUT, module, XF_202::OUTPUT_1 + i));
-			addOutput(Port::create<RedPort>(Vec(92,120 + offset), Port::OUTPUT, module, XF_202::OUTPUTR_1 + i));
+			addOutput(createOutputCentered<SilverPort>(Vec(104.5,105.5 + offset), module, XF_202::OUTPUT_1 + i));
+			addOutput(createOutputCentered<RedPort>(Vec(104.5,132.5 + offset), module, XF_202::OUTPUTR_1 + i));
 
-			addParam(ParamWidget::create<SubSwitch2>(Vec(28, 154.5 + offset), module, XF_202::PARAM_CV_1 + i, 0.0f, 1.0f, 0.0f));
-			addParam(ParamWidget::create<SubSwitch3>(Vec(65, 152 + offset), module, XF_202::PARAM_MODE_1 + i, 0.0f, 2.0f, 0.0f));
-			fader = ParamWidget::create<XF_LightKnob>(Vec(33, 51 + offset), module, XF_202::PARAM_FADE_1 + i, 0.0f, 10.0f, 5.0f);
+			addParam(createParamCentered<SubSwitch2>(Vec(35, 167 + offset), module, XF_202::PARAM_CV_1 + i, 0.0f, 1.0f, 0.0f));
+			addParam(createParamCentered<SubSwitch3>(Vec(72.5, 167 + offset), module, XF_202::PARAM_MODE_1 + i, 0.0f, 2.0f, 0.0f));
+			fader = createParamCentered<XF_LightKnob>(Vec(60, 78 + offset), module, XF_202::PARAM_FADE_1 + i, 0.0f, 10.0f, 5.0f);
 			fader->cv = XF_202::INPUT_CV_1 + i;
 			fader->link = 0;
 			addParam(fader);
 
-			addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(81, 156 + offset), module, XF_202::LIGHT_LIN_1 + i));
-			addChild(ModuleLightWidget::create<TinyLight<BlueLight>>(Vec(81, 166 + offset), module, XF_202::LIGHT_LOG_1 + i));
-			addChild(ModuleLightWidget::create<TinyLight<BlueRedLight>>(Vec(81, 176 + offset), module, XF_202::LIGHT_AUTO_1 + i * 2));
+			addChild(createLightCentered<TinyLight<BlueLight>>(Vec(82.5, 157.5 + offset), module, XF_202::LIGHT_LIN_1 + i));
+			addChild(createLightCentered<TinyLight<BlueLight>>(Vec(82.5, 167.5 + offset), module, XF_202::LIGHT_LOG_1 + i));
+			addChild(createLightCentered<TinyLight<BlueRedLight>>(Vec(82.5, 177.5 + offset), module, XF_202::LIGHT_AUTO_1 + i * 2));
 		}
 
+	}
+	void render(NVGcontext *vg, SchemeCanvasWidget *canvas) override {
+		drawBase(vg, "XF-202");
+		
+		// Dividers
+		nvgStrokeColor(vg, gScheme.alternative);
+		nvgStrokeWidth(vg, 1);
+		nvgLineCap(vg, NVG_ROUND);
+		nvgLineJoin(vg, NVG_ROUND);
+		nvgBeginPath(vg);
+		nvgMoveTo(vg, 3, 190.5);
+		nvgLineTo(vg, 117, 190.5);
+		nvgStroke(vg);
+
+		nvgStrokeColor(vg, gScheme.contrast);
+	
+		for (unsigned int i = 0; i < 2; i++) {
+			nvgBeginPath(vg);
+			nvgMoveTo(vg, 35.000000, 103.500000 + 176 * i);
+			nvgBezierTo(vg, 36.333336, 102.166664 + 176 * i, 37.666664, 100.833336 + 176 * i, 39.000000, 99.500000 + 176 * i);
+			nvgBezierTo(vg, 30.295670, 90.968048 + 176 * i, 27.618584, 78.017834 + 176 * i, 32.226555, 66.733948 + 176 * i);
+			nvgBezierTo(vg, 36.834526, 55.450062 + 176 * i, 47.811501, 48.075714 + 176 * i, 60.000000, 48.075714 + 176 * i);
+			nvgBezierTo(vg, 72.188499, 48.075714 + 176 * i, 83.165482, 55.450069 + 176 * i, 87.773453, 66.733955 + 176 * i);
+			nvgBezierTo(vg, 92.381424, 78.017841 + 176 * i, 89.704330, 90.968056 + 176 * i, 81.000000, 99.500000 + 176 * i);
+			nvgBezierTo(vg, 82.333336, 100.833336 + 176 * i, 83.666664, 102.166664 + 176 * i, 85.000000, 103.500000 + 176 * i);
+			nvgStroke(vg);
+
+			drawText(vg, 34, 113 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 10, gScheme.contrast, "A");
+			drawText(vg, 80, 113 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 10, gScheme.contrast, "B");
+			drawText(vg, 26, 164 + 176 * i, NVG_ALIGN_RIGHT | NVG_ALIGN_BASELINE, 8, gScheme.contrast, "UNI");
+			drawText(vg, 26, 176 + 176 * i, NVG_ALIGN_RIGHT | NVG_ALIGN_BASELINE, 8, gScheme.contrast, "BI");
+			drawText(vg, 30, 36 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 16, gScheme.contrast, "A");
+			drawText(vg, 28, 138 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 16, gScheme.contrast, "CV");
+			drawText(vg, 90, 36 + 176 * i, NVG_ALIGN_RIGHT | NVG_ALIGN_BASELINE, 16, gScheme.contrast, "B");
+			drawText(vg, 90, 138 + 176 * i, NVG_ALIGN_RIGHT | NVG_ALIGN_BASELINE, 18, gScheme.contrast, "\xe2\x86\xa6");
+			drawText(vg, 86, 161 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 8, gScheme.contrast, "LIN");
+			drawText(vg, 86, 170 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 8, gScheme.contrast, "LOG");
+			drawText(vg, 86, 179 + 176 * i, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE, 8, gScheme.contrast, "AUTO");
+		}
 	}
 };
 
