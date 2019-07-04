0427659994773e4b077611d3b869c78c2421c409
david-c14 <10398567+david-c14@users.noreply.github.com>
2019-07-04 16:05:58 +0100

    Remove redundant asset helper

--- src/SubmarineFree.hpp
+++ src/SubmarineFree.hpp
@@ -82,12 +82,4 @@ extern Model *modelXG106;
 
 #include "ComponentLibrary/components.hpp"
 
-struct SubHelper {
-	static std::shared_ptr<Svg> LoadPanel(Plugin *pluginInstance, const char *str, int num) {
-		char workingSpace[100];
-		snprintf(workingSpace, 100, "res/%s%02d.svg", str, num);
-		return APP->window->loadSvg(asset::plugin(pluginInstance, workingSpace));
-	}
-};
-
 #define SUBMENU "\xe2\x96\xb6"
