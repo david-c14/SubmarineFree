dca3814ff30c013c3cc6845179dceb2adc175121
David O'Rourke <david-c14@users.noreply.github.com>
2019-11-20 16:40:25 +0000

    Update DO1.cpp

--- src/DO1.cpp
+++ src/DO1.cpp
@@ -6,8 +6,8 @@ namespace {
 
 	NVGcolor colors[26];
 
-// Based on - Set of 22 Simple, Distinct Colors
-// Thanks to Sacha Trubtskoy
+// Based on - Set of 20 Simple, Distinct Colors
+// Thanks to Sacha Trubetskoy
 // https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/	
 
 	json_t *saveJson() {
