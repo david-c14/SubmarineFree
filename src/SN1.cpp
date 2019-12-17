14d37726988ce6c299e4e539234cbe773f2c4815
david-c14 <10398567+david-c14@users.noreply.github.com>
2019-12-17 19:38:55 +0000

    Sound source working but with bugs

--- src/SN1.cpp
+++ src/SN1.cpp
@@ -44,11 +44,13 @@ namespace {
 struct SN_1 : Module {
 	enum ParamIds {
 		PARAM_FREQ,
+		PARAM_EVOL,
 		PARAM_LENGTH,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		INPUT_FREQ,
+		INPUT_EVOL,
 		NUM_INPUTS
 	};
 	enum OutputIds {
@@ -68,6 +70,7 @@ struct SN_1 : Module {
 	SN_1() : Module() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(PARAM_FREQ, -54.0f, +54.0f, 0.0f, "Frequency", " Hz", dsp::FREQ_SEMITONE, dsp::FREQ_C4);
+		configParam(PARAM_EVOL, 0.0f, 1.0f, 0.0f, "Evolution");
 		configParam(PARAM_LENGTH, 2.0f, maxGridWidth, effectiveGridWidth, "Cycle Length");
 		initLFSR(lfsr);
 		initGridRow();
@@ -114,6 +117,8 @@ struct SN_1 : Module {
 			grid[i].urx = grid[j].ulx;
 			grid[i].ury = grid[j].uly;
 		}
+		grid[(effectiveGridWidth - 1)].urx = grid[0].ulx;
+		grid[(effectiveGridWidth - 1)].ury = grid[0].uly;
 	}
 
 	void resetY() {
@@ -125,15 +130,15 @@ struct SN_1 : Module {
 	void resetX() {
 		int newEffectiveGridWidth = clamp(params[PARAM_LENGTH].getValue(), 2.0f, maxGridWidth);
 		if (effectiveGridWidth != newEffectiveGridWidth) {
-			grid[effectiveGridWidth].lrx = grid[(effectiveGridWidth + 1) % maxGridWidth].llx;
-			grid[effectiveGridWidth].urx = grid[(effectiveGridWidth + 1) % maxGridWidth].ulx;
-			grid[effectiveGridWidth].lry = grid[(effectiveGridWidth + 1) % maxGridWidth].lly;
-			grid[effectiveGridWidth].ury = grid[(effectiveGridWidth + 1) % maxGridWidth].uly;
+			grid[(effectiveGridWidth - 1)].lrx = grid[effectiveGridWidth % maxGridWidth].llx;
+			grid[(effectiveGridWidth - 1)].urx = grid[effectiveGridWidth % maxGridWidth].ulx;
+			grid[(effectiveGridWidth - 1)].lry = grid[effectiveGridWidth % maxGridWidth].lly;
+			grid[(effectiveGridWidth - 1)].ury = grid[effectiveGridWidth % maxGridWidth].uly;
 			effectiveGridWidth = newEffectiveGridWidth;
-			grid[effectiveGridWidth].lrx = grid[0].llx;
-			grid[effectiveGridWidth].urx = grid[0].ulx;
-			grid[effectiveGridWidth].lry = grid[0].lly;
-			grid[effectiveGridWidth].ury = grid[0].uly;
+			grid[(effectiveGridWidth - 1)].lrx = grid[0].llx;
+			grid[(effectiveGridWidth - 1)].urx = grid[0].ulx;
+			grid[(effectiveGridWidth - 1)].lry = grid[0].lly;
+			grid[(effectiveGridWidth - 1)].ury = grid[0].uly;
 		}
 		int intX = (int)x;
 		x -= intX;
@@ -145,9 +150,38 @@ struct SN_1 : Module {
 		float freq = baseFreq * powf(2.0f, params[PARAM_FREQ].getValue() * (1.0f / 12.0f) + (inputs[INPUT_FREQ].isConnected()?inputs[INPUT_FREQ].getVoltage():0.0f));
 		float deltaTime = freq * args.sampleTime;
 		x = x + deltaTime;
-		while (x > effectiveGridWidth)
+		while (x >= effectiveGridWidth)
 			resetX();
-		outputs[OUTPUT_1].setVoltage(x,0);
+		float evol = params[PARAM_EVOL].getValue() + (inputs[INPUT_EVOL].isConnected()?inputs[INPUT_EVOL].getVoltage():0.0f) * 0.1f;
+		evol *= evol;
+		y += clamp(evol, 0.0f, 1.0f) * args.sampleTime;
+		if (y >= 1)
+			resetY();	
+
+		int cell = (int)x;
+		float xoffset = x - cell;
+
+		__m128 dvX = _mm_set_ps(xoffset, xoffset - 1.0f, xoffset, xoffset - 1.0f);
+		__m128 dvY = _mm_set_ps(y, y, y - 1.0f, y - 1.0f);
+
+		dvY = _mm_mul_ps(dvY, _mm_set1_ps(0.5f));
+		dvX = _mm_sub_ps(dvX, dvY);
+		dvY = _mm_mul_ps(dvY, _mm_set1_ps(1.73205080757f));
+
+		__m128 dot = _mm_add_ps(_mm_mul_ps(dvX, _mm_load_ps(&(grid[cell].llx))), _mm_mul_ps(dvY, _mm_load_ps(&(grid[cell].lly))));
+
+		dvX = _mm_mul_ps(dvX, dvX);
+		dvY = _mm_mul_ps(dvY, dvY);
+		dvX = _mm_sub_ps(_mm_set1_ps(0.75f), _mm_add_ps(dvX, dvY));
+		dvX = _mm_max_ps(dvX, _mm_set1_ps(0.0f));
+		dvX = _mm_mul_ps(dvX, dvX);
+		dvX = _mm_mul_ps(dvX, dvX);
+		dvX = _mm_mul_ps(dvX, dot);
+		float output[4];
+		_mm_store_ps(output, dvX);
+		output[0] += (output[1] + output[2] + output[3]);
+
+		outputs[OUTPUT_1].setVoltage(80.0f * output[0],0);
 	}
 };
 
@@ -159,7 +193,9 @@ struct SN101 : SchemeModuleWidget {
 
 		addInput(createInputCentered<SilverPort>(Vec(15,31.5), module, SN_1::INPUT_FREQ));
 		addParam(createParamCentered<SmallKnob<LightKnob>>(Vec(15, 70), module, SN_1::PARAM_FREQ));
-		lengthKnob = createParamCentered<SnapKnob<SmallKnob<LightKnob>>>(Vec(15, 110), module, SN_1::PARAM_LENGTH);
+		addInput(createInputCentered<SilverPort>(Vec(15,111.5), module, SN_1::INPUT_EVOL));
+		addParam(createParamCentered<SmallKnob<LightKnob>>(Vec(15, 150), module, SN_1::PARAM_EVOL));
+		lengthKnob = createParamCentered<SnapKnob<SmallKnob<LightKnob>>>(Vec(15, 190), module, SN_1::PARAM_LENGTH);
 		addParam(lengthKnob);
 		addOutput(createOutputCentered<SilverPort>(Vec(15,350), module, SN_1::OUTPUT_1));
 	}
