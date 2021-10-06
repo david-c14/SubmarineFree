97fac158dd925981c3e673922e979dd76fa9039d
U-OPTIX\david.orourke <david.orourke@Dev-DavidR.optix.co.uk>
2021-10-06 08:56:58 +0100

    Disable majority of modules

--- src/SubmarineFree.hpp
+++ src/SubmarineFree.hpp
@@ -7,17 +7,17 @@ extern Plugin *pluginInstance;
 
 // Forward-declare each Model, defined in each module source file
 
-extern Model *modelAG104;
-extern Model *modelAG106;
-extern Model *modelAG202;
+//extern Model *modelAG104;
+//extern Model *modelAG106;
+//extern Model *modelAG202;
 
-extern Model *modelAO106;
-extern Model *modelAO112;
-extern Model *modelAO118;
-extern Model *modelAO124;
-extern Model *modelAO136;
+//extern Model *modelAO106;
+//extern Model *modelAO112;
+//extern Model *modelAO118;
+//extern Model *modelAO124;
+//extern Model *modelAO136;
 
-extern Model *modelBB120;
+//extern Model *modelBB120;
 
 extern Model *modelBP101;
 extern Model *modelBP102;
@@ -30,90 +30,90 @@ extern Model *modelBP120;
 extern Model *modelBP124;
 extern Model *modelBP132;
 
-extern Model *modelDN112;
+//extern Model *modelDN112;
 
-extern Model *modelDO105;
-extern Model *modelDO110;
-extern Model *modelDO115;
-extern Model *modelDO120;
+//extern Model *modelDO105;
+//extern Model *modelDO110;
+//extern Model *modelDO115;
+//extern Model *modelDO120;
 
-extern Model *modelEN104;
-extern Model *modelEO102;
+//extern Model *modelEN104;
+//extern Model *modelEO102;
 
-extern Model *modelFF110;
-extern Model *modelFF120;
-extern Model *modelFF206;
-extern Model *modelFF212;
+//extern Model *modelFF110;
+//extern Model *modelFF120;
+//extern Model *modelFF206;
+//extern Model *modelFF212;
 
-extern Model *modelHS101;
+//extern Model *modelHS101;
 
-extern Model *modelLA108;
+//extern Model *modelLA108;
 
-extern Model *modelLD103;
-extern Model *modelLD106;
+//extern Model *modelLD103;
+//extern Model *modelLD106;
 
-extern Model *modelLT116;
+//extern Model *modelLT116;
 
-extern Model *modelMZ909;
+//extern Model *modelMZ909;
 
-extern Model *modelNG106;
-extern Model *modelNG112;
-extern Model *modelNG206;
+//extern Model *modelNG106;
+//extern Model *modelNG112;
+//extern Model *modelNG206;
 
-extern Model *modelOA103;
-extern Model *modelOA105;
+//extern Model *modelOA103;
+//extern Model *modelOA105;
 
-extern Model *modelOG104;
-extern Model *modelOG106;
-extern Model *modelOG202;
+//extern Model *modelOG104;
+//extern Model *modelOG106;
+//extern Model *modelOG202;
 
-extern Model *modelPG104;
-extern Model *modelPG112;
+//extern Model *modelPG104;
+//extern Model *modelPG112;
 
-extern Model *modelPO101;
-extern Model *modelPO102;
-extern Model *modelPO204;
+//extern Model *modelPO101;
+//extern Model *modelPO102;
+//extern Model *modelPO204;
 
-extern Model *modelSN101;
+//extern Model *modelSN101;
 
-extern Model *modelSS112;
-extern Model *modelSS208;
-extern Model *modelSS212;
-extern Model *modelSS220;
-extern Model *modelSS221;
+//extern Model *modelSS112;
+//extern Model *modelSS208;
+//extern Model *modelSS212;
+//extern Model *modelSS220;
+//extern Model *modelSS221;
 
-extern Model *modelTD116;
-extern Model *modelTD202;
-extern Model *modelTD316;
-extern Model *modelTD410;
-extern Model *modelTD510;
-extern Model *modelTF101;
-extern Model *modelTF102;
-extern Model *modelTM105;
+//extern Model *modelTD116;
+//extern Model *modelTD202;
+//extern Model *modelTD316;
+//extern Model *modelTD410;
+//extern Model *modelTD510;
+//extern Model *modelTF101;
+//extern Model *modelTF102;
+//extern Model *modelTM105;
 
-extern Model *modelVM101;
-extern Model *modelVM102;
-extern Model *modelVM201;
-extern Model *modelVM202;
+//extern Model *modelVM101;
+//extern Model *modelVM102;
+//extern Model *modelVM201;
+//extern Model *modelVM202;
 
-extern Model *modelWK101;
-extern Model *modelWK205;
+//extern Model *modelWK101;
+//extern Model *modelWK205;
 
-extern Model *modelWM101;
-extern Model *modelWM102;
+//extern Model *modelWM101;
+//extern Model *modelWM102;
 
-extern Model *modelXF101;
-extern Model *modelXF102;
-extern Model *modelXF104;
-extern Model *modelXF201;
-extern Model *modelXF202;
-extern Model *modelXF301;
+//extern Model *modelXF101;
+//extern Model *modelXF102;
+//extern Model *modelXF104;
+//extern Model *modelXF201;
+//extern Model *modelXF202;
+//extern Model *modelXF301;
 
-extern Model *modelXG104;
-extern Model *modelXG106;
-extern Model *modelXG202;
+//extern Model *modelXG104;
+//extern Model *modelXG106;
+//extern Model *modelXG202;
 
-extern Model *modelXX219;
+//extern Model *modelXX219;
 
 #include "shared/components.hpp"
 
